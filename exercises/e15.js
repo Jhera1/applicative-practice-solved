eb8c571938519a864d78beaca27cc1dd99c68b98
Justin Armstrong <armstrongjustin034@gmail.com>
2024-06-12 19:30:07 -0700

    passed test-15

--- exercises/e15.js
+++ exercises/e15.js
@@ -6,6 +6,9 @@ import { data } from "../data/data";
 
 export function getPlanetsWithNoMoons(data) {
   // Your code goes here...
+  return data.planets
+  .filter(planet => !planet.moonsCount || planet.moonsCount === 0)
+  .map(planet => planet.name);
 }
 
 

b806f69673facddcf727bf8a4a5bba7e7f80151f
Justin Armstrong <armstrongjustin034@gmail.com>
2024-06-13 13:20:30 -0700

    passed test-18

--- exercises/e18.js
+++ exercises/e18.js
@@ -7,7 +7,26 @@
 export function getGreatestDiscoveryYear(data) {
   // Your code goes here...
   // feel free to import your `maxBy` or `minBy` methods from previous lessons
-}
+  const asteroidCountsByYear = data.asteroids.reduce((acc, asteroid) => {
+    const year = asteroid.discoveryYear;
+    if (!acc[year]) {
+      acc[year] = 0;
+    }
+    acc[year]++;
+    return acc;
+  }, {});
+
+  
+  const maxYear = Object.keys(asteroidCountsByYear).reduce((maxYear, year) => {
+    return asteroidCountsByYear[year] > asteroidCountsByYear[maxYear] ? year : maxYear;
+  });
+
+  return Number(maxYear);
+  
+  }
+
+
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-18"
